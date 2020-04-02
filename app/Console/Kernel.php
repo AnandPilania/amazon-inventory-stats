05c1f739711cecf9459402b6dbe11a04db5b434f
Adnan Mumtaz <adnanmumtazmayo@gmail.com>
2020-04-02 18:14:18 +0500

    Added commadn in the Kernal

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule (Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('mws:report')->withoutOverlapping()->everyMinute();
     }
 
     /**
