2a8463514d1340ffba0f8dff9fb9d6e4e31c6bd5
Adnan Mumtaz <adnanmumtazmayo@gmail.com>
2020-04-02 19:09:14 +0500

    Fixed query

--- app/Http/Controllers/Amazon/DashboardController.php
+++ app/Http/Controllers/Amazon/DashboardController.php
@@ -86,7 +86,7 @@ class DashboardController extends Controller
                 $count = Order::query()
                     ->where('marketplace_id', $request->marketplace_id)
                     ->whereDate(DB::raw('DATE(purchase_date)'), '=', $header)
-                    ->where('order_status', '=', 'shipped')
+                    ->where('order_status', '!=', 'Cancelled')
                     ->where('sku', $sku)
                     ->selectRaw('SUM(quantity) as  total, DATE_FORMAT(purchase_date, "%Y-%m-%d") as purchase_date')
                     ->groupByRaw('DATE_FORMAT(purchase_date, "%Y-%m-%d")')
